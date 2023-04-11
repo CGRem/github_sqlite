defafa28335aed2cf08da58d2d54da0d28b58292
CGRem <cgrem78@yandex.ru>
2023-04-11 10:18:04 +0300

    добавить цикл for
    Полное описание:
    добавлен цикл

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ int main()
 {
     setlocale (LC_ALL, "Rus");
     cout << "hello world" << endl;
+    // цикл for
+    for (int index_Int = 0; index_Int < 10; index_Int++){
+        cout << "hello world cycle" << endl;
+    }
 
     system("pause");
     return 0;
