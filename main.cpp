44b49d2c8d483197edc4ca2f64fd615c6661f804
CGRem <125661027+CGRem@users.noreply.github.com>
2023-04-29 13:09:14 +0300

    5 сусликов

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
     setlocale (LC_ALL, "Rus");
     cout << "hello world" << endl;
     // цикл for
-    for (int index_Int = 0; index_Int < 10; index_Int++){
+    for (int index_Int = 0; index_Int < 5; index_Int++){
         cout << "hello world cycle" << endl;
         // comment.
     }
